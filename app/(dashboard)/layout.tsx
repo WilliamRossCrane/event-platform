a47cff1adcee6712e17ffff235c42da6806d0454
William Crane <68636614+WilliamRossCrane@users.noreply.github.com>
2026-01-07 04:52:59 +0000

    Feat(Meta Data): Played around with Static and Dynamic meta data, interesting to know you can pull dyanmic objects and info to fill data at runtime thats cool.

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+export const metadata = {
+  title: "Dashboard",
+  description: "Dashboard layout for the application",
+};
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,25 @@
 import React from "react";
 
+export async function generateMetadata() {
+  const title = "Navbar | JS Mastery";
+  const seoDescription =
+    "Free resources database containing ebooks, guides and videos";
+  const imageUrl = "https://example.com/thumbnail.png"; // Replace with your actual image URL
+
+  return {
+    title,
+    description: seoDescription,
+    other: {
+      "og:title": title,
+      "og:description": seoDescription,
+      "og:image": imageUrl,
+      "twitter:title": title,
+      "twitter:description": seoDescription,
+      "twitter:image": imageUrl,
+    },
+  };
+}
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
