c13dbf9f33d541ca2f88a410c91ee08a7caa958a
William Crane <68636614+WilliamRossCrane@users.noreply.github.com>
2026-01-13 21:09:04 +0000

    Feat(ExploreButton Component): Made the basic Explore Button Component for the homepage and added it to Page.tsx.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import ExploreBtn from "@/components/ExploreBtn";
+
 const Page = () => {
   return (
     <section>
       <h1 className="text-center">The Hub for Every Dev <br /> Event you Can't Miss </h1>
       <p className="text-center mt-5">Hackathons, Meetups, and Confrences, All in One Place</p>
+    
+      <ExploreBtn />
     </section>
   );
 };
