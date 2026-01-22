faa44a8a14eaeefc69fdc29de995c12bfe673b7e
William Crane <68636614+WilliamRossCrane@users.noreply.github.com>
2026-01-22 05:21:45 +0000

    Feat(Event Details Page): Started working on events details page added in an event banner and a Event detail object which will be implemented.

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,17 +1,46 @@
 import { notFound } from "next/navigation";
+import Image from "next/image";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const EventDetailItem = ({icon, alt, label}: { icon: string; alt: string; label: string; }) => (
+  <div> 
+    <Image src={icon} alt={alt} width={17} height={17} />
+    <p>{label}</p>
+  </div>
+)
 
 const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const { slug } = await params;
   const request = await fetch(`${BASE_URL}/api/events/${slug}`);
-  const { event } = await request.json();
+  const { event: {description, image, overview, date, time, location, mode, agenda, audience, tags} } = await request.json();
 
-  if (!event) return notFound();
+  if (!description) return notFound();
 
   return (
     <section id="event">
-      <h1>Event Details: <br /> {slug}</h1>
+      <div className="header">
+        <h1>Event Description</h1>;
+        <p className="mt-2">{description}</p>
+      </div>
+
+      <div className="details">
+        {/* Left Side - Event Content  */}
+        <div className="content">
+          <Image src={image} alt="Event Banner" width={800} height={800} className="banner"/>
+
+          <section className="flex-col-gap-2">
+            <h2>Event Details</h2>
+
+          </section>
+
+        </div>
+        {/* Right Side - Booking Form */}
+
+        <aside className="booking">
+          <p className="text-lg font-semibold">Book Event</p>
+        </aside>
+      </div>
     </section>
   );
 }
