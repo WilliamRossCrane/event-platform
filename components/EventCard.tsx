b8759f5edb5dd491b17b4f7b0cb6a1f66a13cfc8
William Crane <wcran7@gmail.com>
2026-01-24 14:35:58 +1000

    Fix(Link Update): Updated Event Cards so they link to a page with the corresponding slug.

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import Link from "next/link";
 import Image from "next/image";
@@ -15,7 +15,7 @@ interface Props {
 
 const EventCard = ({ title, image, slug, location, date, time }: Props) => {
   const handleClick = () => {
-    posthog.capture('event_card_clicked', {
+    posthog.capture("event_card_clicked", {
       event_title: title,
       event_slug: slug,
       event_location: location,
@@ -24,8 +24,14 @@ const EventCard = ({ title, image, slug, location, date, time }: Props) => {
   };
 
   return (
-    <Link href={`/events`} id="event-card" onClick={handleClick}>
-      <Image src={image} alt={title} width={410} height={300} className="poster" />
+    <Link href={`/events/${slug}`} id="event-card" onClick={handleClick}>
+      <Image
+        src={image}
+        alt={title}
+        width={410}
+        height={300}
+        className="poster"
+      />
 
       <div className="flex flex-row gap-2">
         <Image src="/icons/pin.svg" alt="location" width={14} height={14} />
@@ -45,7 +51,7 @@ const EventCard = ({ title, image, slug, location, date, time }: Props) => {
         </div>
       </div>
     </Link>
-  )
-}
+  );
+};
 
-export default EventCard
+export default EventCard;
